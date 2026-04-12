a1d1a4a8de87698a760a228d20f8643108903e31
ayoub elfaqir <ayoub.elfaqir1@gmail.com>
2026-04-12 14:48:33 +0100

    enable cors middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,8 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
+    $middleware->api(prepend: [
+        \Illuminate\Http\Middleware\HandleCors::class,
+    ]);
+})
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (AuthenticationException $exception, Request $request) {
             if ($request->is('api/*') || $request->expectsJson()) {
